8604ebd1f3d46e9dcd8ff534a499503a1e137b70
Pedro Odake <165703800+pe-odake@users.noreply.github.com>
2025-06-16 16:14:51 -0300

    Adicionado codigo para railway

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-
-# main.py
 import flet as ft
+import os
 
 def main(page: ft.Page):
     page.title = "Meu App Móvel"
-    page.add(ft.Text("Teste do Drone com o Tablet"))
+    page.add(ft.Text("Teste do Drone com o Tablet - BANANA"))
 
-ft.app(target=main, view=ft.WEB_BROWSER)
-
+ft.app(target=main, view=ft.WEB_BROWSER, port=int(os.environ.get("PORT", 8501)))
